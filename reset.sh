a8e452c103c3ee909e3e840054e139f0d98a8fba
yonacy <2195149090@qq.com>
2026-01-19 01:34:41 +0800

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo"🛑 Killing CrossOver..."
-pkill CrossOver && echo"✅ CrossOver processes killed."
+echo "🛑 Killing CrossOver..."
+pkill CrossOver && echo "✅ CrossOver processes killed."
 
-echo"🕒 Setting new trial time..."
+echo "🕒 Setting new trial time..."
 DATETIME=$(date -u -v -3H '+%Y-%m-%dT%TZ')
-echo"✅ New trial date: ${DATETIME}"
+echo "✅ New trial date: ${DATETIME}"
 
 defaults write com.codeweavers.CrossOver FirstRunDate -date"${DATETIME}"
 defaults write com.codeweavers.CrossOver SULastCheckTime -date"${DATETIME}"
 killall cfprefsd
 
-echo"🧹 Cleaning bottle flags..."
+echo "🧹 Cleaning bottle flags..."
 find "$HOME/Library/Application Support/CrossOver/Bottles" -type f \( -name ".eval" -o -name ".update-timestamp" \) -execrm -f "{}" +
 
-echo"🧽 Cleaning cxoffice registry block..."
+echo "🧽 Cleaning cxoffice registry block..."
 find "$HOME/Library/Application Support/CrossOver/Bottles" -name system.reg -exec sed -i '''/cxoffice/{N;N;N;N;d;}' {} \;
 
-echo"🚀 Restarting CrossOver..."
+echo "🚀 Restarting CrossOver..."
 CO_APP_PATH="/Applications/CrossOver.app/Contents/MacOS"
 "$CO_APP_PATH/CrossOver" >> /tmp/co_log.log 2>&1 &
 
